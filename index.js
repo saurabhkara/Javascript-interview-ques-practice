b41b1d8f1ca17a67f7ec9343b883040d1159e58f
wovvmac088 <wovvmac088@wovvmac088s-MacBook-Pro.local>
2023-08-20 18:31:55 +0530

    signleton and factory design pattern

--- index.js
+++ index.js
@@ -195,4 +195,213 @@ const arrowFun = () => {
 };
 // arrowFun();
 
+const obj6 = {
+  name: "Arrow function",
+  checkOne: () => console.log(this),
+};
+// obj6.checkOne();
+
+const obj7 = {
+  name: "Outer Object Scope",
+  innerObj: {
+    name: "Inner Object scope",
+    fullFunction: function () {
+      checkOne = () => console.log(this);
+      checkOne();
+      console.log("Full function scope", this);
+    },
+  },
+};
+// obj7.innerObj.fullFunction();
+
 //=================================================
+//Prototype Design pattern
+
+//================================================
+//Var, let and const
+
+{
+  var var1 = "Var one";
+  let let1 = "let one";
+  const const1 = "const one";
+  // console.log(var1, let1, const1);
+}
+// console.log(var1, let1, const1);
+
+//==================================================
+//Rest Parameter and Spread Parameter
+
+const arr2 = [1, 2, 3, 4, 5];
+const arr3 = [6, 7, 8, 9, 10];
+const arr4 = [...arr2, ...arr3];
+const [c1, ...r1] = arr4;
+// console.log(arr4);
+// console.log(c1, r1);
+
+function restExam(a, ...r1) {
+  console.log(a, r1);
+}
+// restExam(1, 2, ...arr3);
+
+const obj8 = {
+  ...arr2,
+};
+const obj9 = {
+  ...obj8,
+};
+// console.log(obj8);
+// console.log(obj9);
+
+//=======================================================
+//Ways to create object in Javascript
+
+const obj10 = {
+  name: "Saurabh",
+};
+
+function CreateObject(name) {
+  this.name = name;
+}
+
+const obj11 = new CreateObject("Saurabh");
+// console.log(obj11);
+const obj12 = new Object({ name: "ram" });
+// console.log(obj12);
+
+class Object13 {
+  constructor(name) {
+    this.name = name;
+  }
+}
+const obj13 = new Object13("Saurabh");
+// console.log(obj13);
+
+//==========================================================
+//Promises
+
+// let promise1 = new Promise((resolve, reject) => {
+//   if (false) {
+//     resolve("Successfuly resolved");
+//   } else {
+//     reject("Rejected for some reasone");
+//   }
+//   console.log("Welcome to Promise code");
+// });
+
+// promise1
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((msg) => {
+//     console.log(msg);
+//   });
+
+//==========================================================
+//Classes
+
+class Abc {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  printDetail() {
+    console.log("Name : ", this.name);
+    console.log("Age : ", this.age);
+  }
+}
+const saurabh = new Abc("Saurabh", 26);
+// saurabh.printDetail();
+
+//===============================================
+//Generator Function
+function* generate() {
+  yield 1;
+}
+const iter = generate();
+// console.log(iter.next());
+// console.log(iter.next());
+
+//=================================================
+//WeakSet-it contains only object
+
+const ws = new WeakSet([{ a: 1 }, { b: 2 }, { c: 3 }]);
+// console.log(ws);
+
+//=================================================
+//WeakMap- Only object
+
+const wm = new WeakMap();
+// console.log(wm.set(abc, "123"));
+// console.log(wm.set(abcd, "123"));
+
+//=======================================================
+//Difference between prototypal and classical inheritance
+
+//=======================================================
+//Singleton Pattern
+
+const singleTon = (function () {
+  let instance = null;
+  function createInstance() {
+    return {
+      name: "Saurabh",
+      age: Math.floor(Math.random() * 100),
+    };
+  }
+  return {
+    getInstance: function () {
+      if (instance) {
+        return instance;
+      } else {
+        return (instance = createInstance());
+      }
+    },
+  };
+})();
+
+// console.log(singleTon.getInstance());
+// console.log(singleTon.getInstance());
+// console.log(singleTon.getInstance());
+// console.log(singleTon.getInstance());
+
+//===================================================
+// Factory Design Pattern
+
+class FullTime {
+  salary = 10000;
+}
+class PartTime {
+  salary = 5000;
+}
+class Freelancer {
+  salary = 5000;
+}
+
+class Factory {
+  employee = null;
+  createEmployee(empType) {
+    switch (empType) {
+      case "fulltime":
+        this.employee = new FullTime();
+        break;
+      case "parttime":
+        this.employee = new PartTime();
+        break;
+      case "freelancer":
+        this.employee = new Freelancer();
+        break;
+      default:
+        break;
+    }
+    this.employee.type = empType;
+    return this.employee;
+  }
+}
+
+const factory = new Factory("");
+// console.log(factory.createEmployee("fulltime"));
+// console.log(factory.createEmployee("parttime"));
+// console.log(factory.createEmployee("freelancer"));
+
+//========================================================
+//Async and await and generator functions
