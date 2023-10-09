bb9dfb916e116648ce9844cbd746e084bb2d3313
wovvmac088 <wovvmac088@wovvmac088s-MacBook-Pro.local>
2023-10-09 14:11:04 +0530

    objects concepts

--- MachineCodeP.js
+++ MachineCodeP.js
@@ -81,3 +81,135 @@ function example5(...params) {
 
 let value = (() => parseInt(Math.random() * 10))();
 // console.log(value);
+
+//=========================================================================
+// Write a function which can return multiple values from a function
+
+function returnObj() {
+  let obj = {
+    name: "Saurabh",
+    address: "Arrah",
+  };
+  return obj;
+}
+// console.log(returnObj());
+
+function returnArr() {
+  return [1, 2];
+}
+// console.log(returnArr());
+
+function* returnMultipleValues() {
+  yield 5;
+  yield 4;
+  yield 6;
+}
+// const gen = returnMultipleValues();
+// console.log(gen.next());
+// console.log(gen.next());
+
+//=======================================================================
+// Write a function which can set default values to the parameters of function when an argument is not passed. Also show how to use exisiting parameters to set the value of another parameter
+
+function example6(a = 3, b = 4, c = a + b) {
+  console.log(a, b, c);
+}
+// example6();
+
+//===========================================================================
+//Show the usage of call, apply and bind with practical example
+
+function example7(a) {
+  console.log("example 7 function called", a);
+}
+
+// example7.call(null, 5);
+// example7.apply(null, [8]);
+// example7.bind(null)(8);
+
+//===========================================================================
+//Show the usage of a function which can be used as a constructor
+
+function example8(name, address) {
+  this.mame = name;
+  this.address = address;
+}
+// const ex8 = new example8("Saurabh", "Arrah");
+// console.log(ex8);
+
+//=========================================================================
+//Show the different ways of creating an object
+
+const objExam1 = {
+  feature: "Object creation",
+};
+const objExam2 = Object.create({ feature: "Object Creation" });
+// console.log(objExam2);
+function ObjectExam3(key, value) {
+  this[key] = value;
+}
+
+const objExam3 = new ObjectExam3("feature", "Object Creation");
+// console.log(objExam3);
+
+class ObjectExam4 {
+  constructor(key, value) {
+    this[key] = value;
+  }
+}
+const obj4 = new ObjectExam4("feature", "Object creation using class");
+// console.log(obj4);
+
+//=========================================================================
+//Display all the keys of an object
+
+let obj5 = {
+  name: "Saurabh",
+  Salary: "1 lac",
+};
+
+function printKey(obj) {
+  for (let key in obj) {
+    console.log("Key :", key, "Value :", obj[key]);
+    // console.log(obj.hasOwnProperty(key));
+  }
+}
+// printKey(obj5);
+
+// for (let key in objExam2) {
+//   console.log(key);
+//   console.log(objExam2.hasOwnProperty(key));
+// }
+
+//=======================================================================
+//Write a function which can check if a given object is empty or not
+
+const emptyObj = {};
+function isObjectEmpty(obj) {
+  let result = false;
+  if (obj !== null && typeof obj !== "undefined" && typeof obj === "object") {
+    result = Object.keys(objExam2).length === 0;
+  }
+  return result;
+}
+// console.log(isObjectEmpty(emptyObj));
+
+//========================================================================
+//Create an empty object which has no prototype attached to it
+
+const obj7 = Object.create(null);
+// console.log(obj7);
+
+//=============================================================================
+//Show the usage of 'Object.entries' to create an object from key value pairs
+
+const entries = [
+  ["name", "Saurabh"],
+  ["salary", "1 lac "],
+];
+
+const objEntries = Object.fromEntries(entries);
+// console.log(objEntries);
+
+//============================================================================
+//
